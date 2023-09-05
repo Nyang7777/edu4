74867cd143aa851a0eb7c1152208650a53366a1b
my <ehdvy2@naver.com>
2023-09-05 12:41:07 +0900

    first commit

---
+++ src/main/java/com/ict/edu4/Edu4Application.java
@@ -0,0 +1,13 @@
+package com.ict.edu4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edu4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edu4Application.class, args);
+	}
+
+}
